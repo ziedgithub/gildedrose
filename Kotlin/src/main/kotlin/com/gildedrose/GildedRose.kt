0966f4a5ff6c472cd1abc4f7d19564d8d216be8f
Zied BEN AMAR <z.benamar@deal.fr>
2021-07-02 16:29:20 +0200

    commit

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,43 +7,47 @@ class GildedRose(var items: Array<Item>) {
         for (item in items) {
             item.decay()
 
-            if (item.name.contains("Conjured")) {
-                val conjured = Items.Conjured(item)
-                conjured.updateQuality()
-                continue
+            updateQuality(item)
+        }
+    }
+
+    private fun updateQuality(item: Item) {
+        if (item.name.contains("Conjured")) {
+            val conjured = Items.Conjured(item)
+            conjured.updateQuality()
+            return
+        }
+
+        if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.decreaseQuality()
             }
 
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.name != "Sulfuras, Hand of Ragnaros") {
-                    item.decreaseQuality()
-                }
+        } else {
+            item.increaseQuality()
 
-            } else {
-                item.increaseQuality()
-
-                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (item.sellIn < 11) {
-                        item.increaseQuality()
-                    }
-
-                    if (item.sellIn < 6) {
-                        item.increaseQuality()
-                    }
-                }
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.name != "Sulfuras, Hand of Ragnaros") {
-                            item.decreaseQuality()
-                        }
-                    } else {
-                        item.quality = 0
-                    }
-                } else {
+            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.sellIn < 11) {
                     item.increaseQuality()
                 }
+
+                if (item.sellIn < 6) {
+                    item.increaseQuality()
+                }
+            }
+        }
+
+        if (item.sellIn < 0) {
+            if (item.name != "Aged Brie") {
+                if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.decreaseQuality()
+                    }
+                } else {
+                    item.quality = 0
+                }
+            } else {
+                item.increaseQuality()
             }
         }
     }
